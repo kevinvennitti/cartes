e10f61521d35571531e642f88a3bd7efa1a65791
Kévin VENNITTI <kevin.vennitti@gmail.com>
2024-08-13 16:22:58 +0200

    Better styles

--- app/tags/Brand.tsx
+++ app/tags/Brand.tsx
@@ -1,8 +1,11 @@
 import css from '@/components/css/convertToJs'
 
 export default function Brand({ brand, brandWikidata, brandWikipedia }) {
+
 	if (!(brand || brandWikidata || brandWikipedia)) return null
+
 	if (!brandWikipedia) return <div>Marque: {brand}</div>
+
 	const [presumedLang, presumedName] = brandWikipedia.split(':'),
 		lang = presumedName ? presumedLang : 'fr',
 		name = presumedName || presumedLang,
@@ -10,66 +13,52 @@ export default function Brand({ brand, brandWikidata, brandWikipedia }) {
 
 	return (
 		<>
-		<div css={`
-			display:flex;
-			align-items: center;
-			margin-top: 1rem;
-			padding-top:1rem;
-			border-top:solid 1px var(--separatorColor);
-		`}>
+		<div 
+			css={`
+				display:flex;
+				align-items: center;
+			`}
+		>
 			<span css={`
 				color:var(--lighterTextColor);	
 			`}>
 				Marque :&nbsp;
 			</span>
-			<div css={`
-				display:flex;
-				align-items: center;
-				gap:4px;
-			`}>
-				<img
+
+			<a 
+				href={url} 
+				target="_blank" 
+				className="link">
+					
+				<img 
 					src={'/wikipedia.svg'}
 					alt="Logo de Wikipedia"
 					width="20"
 					height="20"
-					style={css`
-						vertical-align: middle;
-					`}/>
-					<a href={url} target="_blank" css={`
-						text-decoration:none;
-					`}>
-						{brand}
-					</a>
-				</div>
-			</div>
-			{brandWikidata && 
-			<div>
-				<Wikidata id={brandWikidata} />
-			</div>
-			}
+					/>
+					{brand}
+			</a>
+		</div>
+		{brandWikidata && 
+		<div>
+			<Wikidata id={brandWikidata} />
+		</div>
+		}
 		</>
 	)
 }
 
 export const Wikidata = ({ id }) => (
-	<span
-	css={`
-		display:flex;
-		align-items: center;
-		gap:6px;
-	`}
+	<a 
+		href={`https://wikidata.org/wiki/${id}`} target="_blank"
+		className="link emphasis"
 	>
 		<img
 			src={'/wikidata.svg'}
 			alt="Logo de Wikidata"
 			width="20"
 			height="20"
-			style={css`
-				vertical-align: middle;
-			`}
 		/>{' '}
-		<a href={`https://wikidata.org/wiki/${id}`} target="_blank">
-			<small>wikidata</small>
-		</a>
-	</span>
+		<small>wikidata</small>
+	</a>
 )
