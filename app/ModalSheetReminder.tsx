cd25aaf15e6f09945f1ec918b17fbcd28851a016
Kévin VENNITTI <kevin.vennitti@gmail.com>
2024-08-13 16:24:50 +0200

    Update ModalSheetReminder

--- app/ModalSheetReminder.tsx
+++ app/ModalSheetReminder.tsx
@@ -1,31 +1,21 @@
-import Emoji from '@/components/Emoji'
+import Image from 'next/image'
 
 export const modalSheetBoxShadow = `box-shadow: rgba(0, 0, 0, 0.3) 0px -2px 16px;`
 const popSize = 6
 export default function ModalSheetReminder({ setOpen }) {
 	return (
 		<div
-			css={`
-				background: red;
-				position: fixed;
-				bottom: -${popSize / 2}rem;
-				left: -${popSize / 2}rem;
-				background: var(--lightestColor);
-				width: ${popSize}rem;
-				height: ${popSize}rem;
-				border-radius: ${popSize}rem;
-				border: 2px solid var(--color);
-				${modalSheetBoxShadow}
-				cursor: pointer;
-				> span {
-					position: absolute;
-					top: 0.9rem;
-					right: 0.9rem;
-				}
-			`}
+			className="modalsheet-reminder"
 			onClick={() => setOpen(true)}
 		>
-			<Emoji e="🔎" />
+			
+			<Image 
+				src="/ui/search.svg" 
+				alt="Icône Rechercher"
+				width="24"
+				height="24"
+			/>
+
 		</div>
 	)
 }
